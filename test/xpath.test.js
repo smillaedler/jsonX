e9e7abeac922a064a7edbc5e8bf3cd8bcfa977eb
Matt Handler <matt.handler@gmail.com>
2011-09-30 19:37:39 -0400

    all tests are passing

--- test/xpath.test.js
+++ test/xpath.test.js
@@ -27,9 +27,10 @@ vows.describe('XPath Parser').addBatch({
 				object[0].type.should.equal("absolute");
 			},
 			'we get a path with one step': function (object) {
-				should.equal(object[0].step.test, "node");
-				should.equal(object[0].step.specifier, "");
-				should.equal(object[0].step.predicates, "");
+				should.equal(object[0].step.test.constructor, Object);
+				object[0].step.test.test.should.equal("node");
+				should.equal(object[0].step.specifier.constructor, Object);
+				should.equal(object[0].step.predicates.constructor, Array);
 			}
 		},
 		'parsing root relative': {
@@ -43,9 +44,10 @@ vows.describe('XPath Parser').addBatch({
 				object[0].type.should.equal("relative");
 			},
 			'we get a path with one step': function (object) {
-				should.equal(object[0].step.test, "node");
-				should.equal(object[0].step.specifier, "");
-				should.equal(object[0].step.predicates, "");
+				should.equal(object[0].step.test.constructor, Object);
+				object[0].step.test.test.should.equal("node");
+				should.equal(object[0].step.specifier.constructor, Object);
+				should.equal(object[0].step.predicates.constructor, Array);
 			}
 		},
 		'parsing multiple successive nodes': {
@@ -54,9 +56,43 @@ vows.describe('XPath Parser').addBatch({
 			},
 			'we get a list with two paths': function (array) {
 				array.length.should.equal(2);
-				array[0].step.test.should.equal("node1");
-				array[1].step.test.should.equal("node2");
+				array[0].step.test.test.should.equal("node1");
+				array[1].step.test.test.should.equal("node2");
 			}
-		}
+		},
+		'parsing a legit axis specifier': {
+			topic: function () {
+				return parser.parse("//ancestor::*");
+			},
+			'we get an axis specifier': function (array) {
+				should.equal(array[0].step.specifier.name, "axisSpecifier");
+				array[0].step.specifier.type.should.equal("ancestor"); 
+			}
+		},
+		'parsing an abbreviated axis "@"': {
+			topic: function () {
+				return parser.parse("//@node");
+			},
+			'we get an attribute axis': function (array) {
+				should.equal(array[0].step.specifier.name, "axisSpecifier");
+				array[0].step.specifier.type.should.equal("attribute"); 
+			}
+		},
+		'parsing an abbreviated step ".."': {
+			topic: function () {
+				return parser.parse("/node/..");
+			},
+			'we get a parent axis': function (array) {
+				array[1].step.specifier.type.should.equal("parent"); 
+			}
+		},
+		'parsing an abbreviated step "."': {
+			topic: function () {
+				return parser.parse("/node/.");
+			},
+			'we get a parent axis': function (array) {
+				array[1].step.specifier.type.should.equal("self"); 
+			}
+		}		
 	}
 }).export(module);
